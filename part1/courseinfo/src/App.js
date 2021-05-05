67b3e43513422cb2c32102ca080ec2fccba0f481
mkarkaus <miikka.karkaus@gmail.com>
2021-05-05 14:40:19 +0300

    part1/b/ex03 done

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -19,9 +19,9 @@ const Part = (props) => {
 const Content = (props) => {
 	return (
 		<div>
-			<Part name={props.name1} exercises={props.exercises1} />
-			<Part name={props.name2} exercises={props.exercises2} />
-			<Part name={props.name3} exercises={props.exercises3} />
+			<Part name={props.part1.name} exercises={props.part1.exercises} />
+			<Part name={props.part2.name} exercises={props.part2.exercises} />
+			<Part name={props.part3.name} exercises={props.part3.exercises} />
 		</div>
 	)
 }
@@ -35,21 +35,44 @@ const Header = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+	const course = 'Half Stack application development'
+	const part1 = {
+	  name: 'Fundamentals of React',
+	  exercises: 10
+	}
+	const part2 = {
+	  name: 'Using props to pass data',
+	  exercises: 7
+	}
+	const part3 = {
+	  name: 'State of a component',
+	  exercises: 14
+	}
+  
+	return (
+		<div>
+			<Header course={course} />
+			<Content part1={part1} part2={part2} part3={part3} />
+			<Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+		</div>
+	)
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content name1={part1} name2={part2} name3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-	  <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-    </div>
-  )
+
+//   const course = 'Half Stack application development'
+//   const part1 = 'Fundamentals of React'
+//   const exercises1 = 10
+//   const part2 = 'Using props to pass data'
+//   const exercises2 = 7
+//   const part3 = 'State of a component'
+//   const exercises3 = 14
+
+//   return (
+//     <div>
+//       <Header course={course} />
+//       <Content name1={part1} name2={part2} name3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+// 	  <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+//     </div>
+//   )
 }
 
 export default App
