ab6c5708c1b61c1e765c7c5383cfaa62b7365178
mkarkaus <miikka.karkaus@gmail.com>
2021-05-06 13:12:39 +0300

    part1/d/ex10 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,36 +2,31 @@ import React, { useState } from 'react'
 
 const Header = ({ text }) => <h1>{text}</h1>
 
+const Statistic = ({ name, value }) => <p>{name} {value}</p>
+
 const Statistics = (props) => {
 	const good = props.good
 	const neutral = props.neutral
 	const bad = props.bad
-	const all = props.all
-	const average = props.average
-	const positive = props.positive
-	
+
+	const average = (good, neutral, bad) => (good + (bad * -1)) / (good + neutral + bad)
+	const positive = (good, all) => (good * 100 / all) + ' %'
+
 	if (good + neutral + bad === 0)
 		return (<p>No feedback given</p>)
 	return (
 		<div>
-			<p>good {good}</p>
-			<p>neutral {neutral}</p>
-			<p>bad {bad}</p>
-			<p>all {all}</p>
-			<p>average {average}</p>
-			<p>positive {positive}</p>
+			<Statistic name='good' value={good} />
+			<Statistic name='neutral' value={neutral} />
+			<Statistic name='bad' value={bad} />
+			<Statistic name='all' value={good + neutral + bad} />
+			<Statistic name='average' value={average(good, neutral, bad)} />
+			<Statistic name='positive' value={positive(good, good + neutral + bad)} />
 		</div>
 	)
 }
 
-const Button = ({ handleClick, text }) => {
-	
-	return (
-		<button onClick={handleClick}>
-			{text}
-		</button>
-	)
-}
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
 
 const App = () => {
 	// save clicks of each button to its own state
@@ -41,22 +36,6 @@ const App = () => {
 	const give = 'give feedback'
 	const stats = 'statistics'
 
-	const average = (good, neutral, bad) => {
-		if (good + neutral + bad === 0)
-			return (0)
-		return (
-			((good * 1) + (neutral * 0) + (bad * -1)) / (good + neutral + bad)
-		)
-	}
-
-	const positive = (good, all) => {
-		if (all === 0)
-			return ('0 %')
-		return (
-			(good * 100 / all) + ' %'
-		)
-	}
-
 	return (
 		<div>
 			<Header text={give} />
@@ -64,13 +43,7 @@ const App = () => {
 			<Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
 			<Button handleClick={() => setBad(bad + 1)} text="bad" />
 			<Header text={stats} />
-			<Statistics good={good}
-				neutral={neutral}
-				bad={bad}
-				all={good + neutral + bad}
-				average={average(good, neutral, bad)}
-				positive={positive(good, good + neutral + bad)}
-			/>
+			<Statistics good={good} neutral={neutral} bad={bad} />
 		</div>
 	)
 }
