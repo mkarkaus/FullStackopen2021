6aeeec04e9adcd0fd6c90b111d7243971ea0a670
mkarkaus <miikka.karkaus@gmail.com>
2021-05-07 13:39:17 +0300

    part2/a/ex02 done

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,37 +1,31 @@
 import React from 'react';
 
-const Header = ({ course }) => {
-	return (
-		<h1>{course.name}</h1>
-	)
-}
+const Header = ({ course }) => <h1>{course.name}</h1>
 
-const Part = (props) => {
-	return (
-		<p>
-			{props.part.name} {props.part.exercises}
-		</p>    
-	)
-}
+const Part = (props) => <p>{props.part.name} {props.part.exercises}</p>
 
-const Content = ({ course }) => {
-	return (
+const Content = ({ course }) => (
 		<div>
 			{course.parts.map(part => 
 				<Part key={part.id} part={part} />
 			)}
 		</div>
-	)
-}
+)
 
-const Course = ({ course }) => {
-	return (
+const Total = ({ course }) => (
+		<h3>
+			total of {course.parts.reduce((total, part) => 
+				total + part.exercises, 0)} exercises
+		</h3>
+)
+
+const Course = ({ course }) => (
 		<div>
 			<Header course={course} />
 			<Content course={course} />
+			<Total course={course} />
 		</div>
-	)
-}
+)
 
 const App = () => {
 	const course = {
@@ -52,6 +46,11 @@ const App = () => {
 				name: 'State of a component',
 				exercises: 14,
 				id: 3
+			},
+			{
+				name: 'Redux',
+				exercises: 11,
+				id: 4
 			}
 		]
 	}
