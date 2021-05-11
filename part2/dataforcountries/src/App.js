be28f9b087287823b30d4202a459c111cf753b26
mkarkaus <miikka.karkaus@gmail.com>
2021-05-11 12:34:04 +0300

    part2/c/ex12 done with handled edge cases

---
+++ part2/dataforcountries/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import Filter from './components/Filter'
+import Countries from './components/Countries'
+
+const App = () => {
+	const [found, setFound] = useState([])
+	const [countries, setCountries] = useState([])
+	const [filter, setFilter] = useState('')
+
+	useEffect(() => {
+		axios
+			.get('https://restcountries.eu/rest/v2/all')
+			.then(response => {
+				setCountries(response.data)
+			})
+	}, [])
+
+	useEffect(() => {
+		const filtered = countries.filter(country => 
+			country.name.toLowerCase() === filter.toLowerCase())
+		if (filtered.length === 1)
+			setFound(filtered)
+		else
+			setFound(countries.filter(country => 
+				country.name
+				.toLowerCase()
+				.includes(filter.toLowerCase())
+			))
+	}, [filter, countries])
+
+	return (
+		<div>
+			<Filter filter={filter} setFilter={setFilter} />
+			<Countries found={found} filter={filter} />
+		</div>
+	);
+}
+
+export default App;
---
+++ part2/dataforcountries/src/components/Countries.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const AdditionalInfo = ({ country }) => 
+	<div>
+		<h2>{country.name}</h2>
+		<div>capital {country.capital}</div>
+		<div>population {country.population}</div>
+		<h3>languages</h3>
+		<ul>
+			{country.languages.map((language) =>
+				<li key={language.name} >{language.name}</li>
+			)}
+		</ul>
+		<img src={country.flag}
+			alt="Country flag"
+			height="100px"
+			border="2px solid black"
+		/>
+	</div>
+
+const Country = ({ country }) => <div>{country.name}</div>
+
+const Countries = ({ found, filter }) => {
+	if (found.length > 10)
+		return (<div>Too many matches, specify another filter</div>)
+	else if (found.length === 1)
+		return (<AdditionalInfo country={found[0]} />)
+	return (
+		<div>
+			{found.map(country => 
+				country.name.toLowerCase().includes(filter.toLowerCase())
+				? <Country key={country.name} country={country} />
+				: null
+			)}
+		</div>
+	)
+}
+
+export default Countries
